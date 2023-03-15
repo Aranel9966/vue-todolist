ad65874efe76bd44bd1bac68ea4376b886d05190
Aranel9966 <99angelo.dire@gmail.com>
2023-03-15 15:26:40 +0100

    add MILESTONE 3

--- js/script.js
+++ js/script.js
@@ -10,7 +10,8 @@ Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
 MILESTONE 2
 Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
 MILESTONE 3
-Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo,
+ che quindi viene aggiunto alla lista dei todo esistenti.
 Bonus:
 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
@@ -44,7 +45,10 @@ const { createApp } = Vue
             },
         ],
 
-        // indexTodo:0
+        newTodo:{
+            text:'',
+            done:false,
+        },
 
       }
     },
@@ -59,8 +63,11 @@ const { createApp } = Vue
 
         removeTodo(indexTodo){
             this.todoList.splice(indexTodo,1)
-            console.log(indexTodo)
-            console.log(this.todoList)
+        },
+
+        addTodo(){
+            this.todoList.push(this.newTodo)
+            this.newTodo={}
         },
         
     },
